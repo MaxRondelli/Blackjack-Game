d8cb3d5a4a749d6ef8da4df99a2b7130b44ba502
MaxRondelli <83310259+MaxRondelli@users.noreply.github.com>
2022-10-04 11:07:36 +0200

    Created Discord Bot

---
+++ Discord Bot/Bot.py
@@ -0,0 +1,23 @@
+import os
+import discord
+from dotenv import load_dotenv
+    
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client(intents = discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')     
+   
+   
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+       return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+        
+client.run(TOKEN)
